d10961e4f21eeeb55055b5e6fd9778a2fab64456
Andres <andrese-rios@utp.edu.co>
2023-07-12 18:32:12 -0500

    add format to time data and structure /home design

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 
 
@@ -28,7 +29,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(utc_offSet)));
       
       // set the variable
-      time = now.toString();
+      time = DateFormat.jm().format(now);
       print('now is $now and time is $time'); 
     
   } catch (e) {
