062936b72fde385f4d0a33a6908d2371d0438b94
Andres <andrese-rios@utp.edu.co>
2023-07-12 10:32:04 -0500

    add routes and navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:world_time/pages/home.dart'; 
-void main() => runApp(MaterialApp(
-  home: SafeArea(child: Home()),
+import 'package:world_time/pages/choose_location.dart';
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart'; 
+void main() => runApp(MaterialApp(  
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation()
+  },
 ));
